31c138f1ac5e117b0ed9d3d67346c391c54dfd16
VIV <ilja.vyazmin@mail.ru>
2017-05-25 20:31:17 +0300

    second_try

--- posts/posts/settings.py
+++ posts/posts/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_n-hz0rll$j9kf3kph7wwe7cghsw6y)d2$yugqux7&y83m%%t2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
